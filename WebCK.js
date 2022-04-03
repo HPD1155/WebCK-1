a9c95f51877aa53f98455064650bb2d4ad29d1ef
Hack.cpp <72662612+HPD1155@users.noreply.github.com>
2022-04-03 16:40:22 -0500

    Update WebCK.js

---
+++ WebCK.js
@@ -0,0 +1,8 @@
+function handletester(handle)
+{
+  while i <= handle {
+    console.log("check!");
+  }
+  console.clear();
+  alert("If you got this message, that means your computer can handle this website");
+}
